c5bb279c691aa12fb9e1de6da1880715afac721d
QuantischerPiepvogel <32911370+QuantischerPiepvogel@users.noreply.github.com>
2017-10-19 23:58:25 +0200

    Update MainControl.py

--- MainControl.py
+++ MainControl.py
@@ -25,10 +25,10 @@ frame.pack()
 bottomframe = Frame(root)
 bottomframe.pack( side = BOTTOM )
 
-exit_button = Button(frame, text="X", fg="black", command="software_exit()", width=50, height=50, x=10, y=10)
+exit_button = Button(frame, text="X", fg="black", command="software_exit()", width=50, height=50)
 #exit_button.place(10, 10)
 
-update_button = Button(frame, text="U", fg="black", command="software_update()", width=50, height=50, x=60, y=10)
+update_button = Button(frame, text="U", fg="black", command="software_update()", width=50, height=50)
 #update_button.place(60, 10)
 
 root.mainloop()
