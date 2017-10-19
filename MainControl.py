23c25504ae73071c57e73ee1ecc209c65530b6d0
QuantischerPiepvogel <32911370+QuantischerPiepvogel@users.noreply.github.com>
2017-10-19 23:51:40 +0200

    Update MainControl.py

--- MainControl.py
+++ MainControl.py
@@ -1,1 +1,42 @@
+from Tkinter import *
+import os
+
+def software_exit():
+  
+  if messagebox.askokcancel("Question","Do you really want to close the program?"):
+    
+    exit()
+  
+def software_update():
+  
+  if messagebox.askokcancel("Question","Do you really want to update the program?"):
+    
+    
+    os.system("cd RemoteControl")
+    os.system("sudo python Updater.py")
+    
+    exit()
+  
+
+root = Tk()
+frame = Frame(root)
+frame.pack()
+
+bottomframe = Frame(root)
+bottomframe.pack( side = BOTTOM )
+
+exit_button = Button(frame, text="X", fg="black")
+exit_button.command(software_exit())
+exit_button.width(50)
+exit_button.height(50)
+exit_button.place(10, 10)
+
+update_button = Button(frame, text="U", fg="black")
+update_button.command(software_update())
+update_button.width(50)
+update_button.height(50)
+update_button.place(60, 10)
+
+root.mainloop()
+
 print("RemoteControl.py was sucessfully started")
