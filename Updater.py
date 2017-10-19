38f401b5170eedda7d0bc713c2102e0e0646c2fd
QuantischerPiepvogel <32911370+QuantischerPiepvogel@users.noreply.github.com>
2017-10-19 22:47:21 +0200

    Update Updater.py

--- Updater.py
+++ Updater.py
@@ -1,4 +1,9 @@
 # info: https://www.youtube.com/watch?v=FV7eiqN01hc
-os.chdir(os.getcwd())
-git pull
+import subprocess
+subprocess.call(["cd", "RemoteControl"])
+subprocess.call(["git", "pull"])
+
+#cd RemoteControl
+#os.chdir(os.getcwd())
+#git pull
 sudo python MainControl.py
