9a7bc812a57b318b9412520d944cbc1449f14f0f
QuantischerPiepvogel <32911370+QuantischerPiepvogel@users.noreply.github.com>
2017-10-19 23:04:25 +0200

    Update Updater.py
    got it fully working

--- Updater.py
+++ Updater.py
@@ -1,16 +1,8 @@
 
 # info: https://www.youtube.com/watch?v=FV7eiqN01hc
-#import subprocess
+
 import os
-#subprocess.call(["cd", "RemoteControl"])
-#subprocess.call(["git", "pull"])
-#subprocess.call(["sudo", "python", "MainControl.py"])
-#cd RemoteControl
-#os.chdir(os.getcwd())
-#subprocess.call(["git", "pull"])
+
 os.system("cd RemoteControl")
 os.system("git pull")
 os.system("sudo python MainControl.py")
-#git pull
-#sudo python MainControl.py
-#55
