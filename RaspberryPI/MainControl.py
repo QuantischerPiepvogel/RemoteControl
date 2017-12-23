1b4f3cdf9acb0533d1a17b65ab14744745f07493
QuantischerPiepvogel <32911370+QuantischerPiepvogel@users.noreply.github.com>
2017-12-23 20:05:25 +0100

    Update MainControl.py

--- RaspberryPI/MainControl.py
+++ RaspberryPI/MainControl.py
@@ -29,28 +29,25 @@ def software_update():
     #os.system("sudo python RaspberryPI/Updater.py")
     
     #exit()
-  
-def onKeyPress():
-  
-  print("loool")
 
-class FullScreenApp(object):
-    def __init__(self, master, **kwargs):
-        self.master=master
-        pad=3
-        self._geom='200x200+0+0'
-        master.geometry("{0}x{1}+0+0".format(
-            master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad))
-        master.bind('<Escape>',self.toggle_geom)            
-    def toggle_geom(self,event):
-        geom=self.master.winfo_geometry()
-        print(geom,self._geom)
-        self.master.geometry(self._geom)
-        self._geom=geom
+#class FullScreenApp(object):
+    #def __init__(self, master, **kwargs):
+        #self.master=master
+        #pad=3
+        #self._geom='200x200+0+0'
+        #master.geometry("{0}x{1}+0+0".format(
+            #master.winfo_screenwidth()-pad, master.winfo_screenheight()-pad))
+        #master.bind('<Escape>',self.toggle_geom)            
+    #def toggle_geom(self,event):
+        #geom=self.master.winfo_geometry()
+        #print(geom,self._geom)
+        #self.master.geometry(self._geom)
+        #self._geom=geom
   
 root = tk.Tk()
 root.title("MainControl")
-app=FullScreenApp(root)
+root.bind('<Escape>',self.software_exit)
+#app=FullScreenApp(root)
 root.bind
 #root.attributes('-fullscreen', True)
 root.mainloop()
