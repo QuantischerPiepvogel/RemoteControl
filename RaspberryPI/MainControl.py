a5a70fcc47e92087a0e8ed670790fcb309f4061a
QuantischerPiepvogel <32911370+QuantischerPiepvogel@users.noreply.github.com>
2017-12-23 21:16:04 +0100

    Update MainControl.py

--- RaspberryPI/MainControl.py
+++ RaspberryPI/MainControl.py
@@ -20,14 +20,8 @@ root.bind('<F5>',software_update)
 root.attributes('-fullscreen', True)
 root.mainloop()
 
-#frame.width = 100
-#frame.height = 100
-
-#bottomframe = Frame(root)
-#bottomframe.pack( side = BOTTOM )
-
-#exit_button = Button(frame, text="X", fg="black", command="software_exit()", width=50, height=50)
-#exit_button.place(10, 10)
+exit_button = Button(root, text="X", fg="black", command="software_exit", width=50, height=50)
+exit_button.place(10, 10)
 
 #update_button = Button(frame, text="U", fg="black", command="software_update()", width=50, height=50)
 #update_button.place(60, 10)
