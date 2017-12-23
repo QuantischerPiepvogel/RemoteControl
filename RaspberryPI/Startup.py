68b693cb24a9f93ebe86f82a115b2310128ceec3
QuantischerPiepvogel <32911370+QuantischerPiepvogel@users.noreply.github.com>
2017-12-23 22:26:02 +0100

    Update Startup.py

--- RaspberryPI/Startup.py
+++ RaspberryPI/Startup.py
@@ -1,11 +1,16 @@
 import time, os
 import RPi.GPIO as GPIO
 
+print("Starting up XD")
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(33, GPIO.IN)
 
-while GPIO.input(33) == GPIO.HIGH:
 
-  time.sleep(0.5)
+
+while GPIO.input(33) == GPIO.HIGH:
   
+  time.sleep(0.5)
+
+print("Button has been pressed")
 os.system("sudo python RaspberryPI/MainControl.py")
