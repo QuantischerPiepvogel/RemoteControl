ad15b15d0e192f582aac19b3a6392e6414d5032c
QuantischerPiepvogel <32911370+QuantischerPiepvogel@users.noreply.github.com>
2018-02-14 18:48:41 +0100

    Update Test.py

--- RaspberryPI/Test.py
+++ RaspberryPI/Test.py
@@ -3,20 +3,20 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(8, GPIO.OUT) #Clock
-GPIO.setup(10, GPIO.OUT) #Latch
+GPIO.setup(18, GPIO.OUT) #Clock
+GPIO.setup(16, GPIO.OUT) #Latch
 GPIO.setup(12, GPIO.OUT) #Data
 
 def shiftOne(bool):
   print("Set to: " + str(bool))
   GPIO.output(12, bool)
   time.sleep(0.01)
-  GPIO.output(8, GPIO.HIGH)
+  GPIO.output(18, GPIO.HIGH)
   time.sleep(0.01)
-  GPIO.output(10, GPIO.HIGH)
+  GPIO.output(16, GPIO.HIGH)
   time.sleep(0.01)
-  GPIO.output(8, GPIO.LOW)
-  GPIO.output(10, GPIO.LOW)
+  GPIO.output(18, GPIO.LOW)
+  GPIO.output(16, GPIO.LOW)
   time.sleep(0.01)
 
 while True:
