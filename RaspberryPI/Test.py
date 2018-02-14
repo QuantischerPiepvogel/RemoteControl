77c5aa5fa960d4581267d80f09ae7e941148fd27
QuantischerPiepvogel <32911370+QuantischerPiepvogel@users.noreply.github.com>
2018-02-14 18:53:34 +0100

    Update Test.py

--- RaspberryPI/Test.py
+++ RaspberryPI/Test.py
@@ -7,17 +7,20 @@ GPIO.setup(18, GPIO.OUT) #Clock
 GPIO.setup(16, GPIO.OUT) #Latch
 GPIO.setup(12, GPIO.OUT) #Data
 
-def shiftOne(bool):
-  print("Set to: " + str(bool))
-  GPIO.output(12, bool)
-  time.sleep(0.01)
-  GPIO.output(18, GPIO.HIGH)
-  time.sleep(0.01)
+
+def addRGB(r, g, b):
+  shiftOne(r)
+  shiftOne(g)
+  shiftOne(b)
+
+def refresh():
   GPIO.output(16, GPIO.HIGH)
-  time.sleep(0.01)
-  GPIO.output(18, GPIO.LOW)
   GPIO.output(16, GPIO.LOW)
-  time.sleep(0.01)
+
+def shiftOne(bool):
+  GPIO.output(12, bool)
+  GPIO.output(18, GPIO.HIGH)
+  GPIO.output(18, GPIO.LOW)
 
 while True:
   shiftOne(GPIO.HIGH)
