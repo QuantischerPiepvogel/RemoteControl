ad1ddb6e2b22149ad1a3a44f616e8888ea5aa62e
QuantischerPiepvogel <32911370+QuantischerPiepvogel@users.noreply.github.com>
2018-02-24 13:39:40 +0100

    farbe wieder grün

--- RaspberryPI/MainControl.py
+++ RaspberryPI/MainControl.py
@@ -65,7 +65,7 @@ def workerThread(*args):
 root = Tk()
 app = App(root)
 root.title("MainControl")
-root.config(bg="red")
+root.config(bg="green")
 root.bind('<F4>',software_exit) #http://effbot.org/tkinterbook/tkinter-events-and-bindings.htm    key bindings
 root.bind('<F5>',software_update)
 root.attributes('-fullscreen', True)
