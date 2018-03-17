597dd713c584a8345f3b7ec5cac888ca45a006e7
QuantischerPiepvogel <32911370+QuantischerPiepvogel@users.noreply.github.com>
2018-03-17 11:26:12 +0100

    Update i2c.py

--- RaspberryPI/i2c.py
+++ RaspberryPI/i2c.py
@@ -4,7 +4,7 @@ import smbus
 
 bus = smbus.SMBus(1)    # 0 = /dev/i2c-0 (port I2C0), 1 = /dev/i2c-1 (port I2C1)
 
-DEVICE_ADDRESS = 9      #7 bit address (will be left shifted to add the read write bit)
+DEVICE_ADDRESS = 0x09      #7 bit address (will be left shifted to add the read write bit)
 DEVICE_REG_MODE1 = 0x00
 DEVICE_REG_LEDOUT0 = 0x1d
 
