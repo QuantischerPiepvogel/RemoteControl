04a0b6351fbe1ab8bdcdf4cf8d56e984d88e563f
rfha <rfha@gmx.de>
2018-04-01 00:51:00 +0200

    Update MainControl.py

--- RaspberryPI/MainControl.py
+++ RaspberryPI/MainControl.py
@@ -31,11 +31,11 @@ class App:
     self.frame.rowconfigure(1, weight=1, minsize=564)
 
     
-    self.home_icon=PhotoImage(file="/images/Taskbar/Home_Icon.png")
-    self.update_icon=PhotoImage(file="/images/Taskbar/Update_Icon.png")
-    self.exit_icon = PhotoImage(file="/images/Taskbar/Exit_Icon.png")
-    self.settings_icon = PhotoImage(file="/images/Taskbar/Settings_Icon.png")
-    self.sensors_icon = PhotoImage(file="/images/Taskbar/Sensors_Icon.png")
+    self.home_icon=PhotoImage(file="RaspberryPI/images/Taskbar/Home_Icon.png")
+    self.update_icon=PhotoImage(file="RaspberryPI/images/Taskbar/Update_Icon.png")
+    self.exit_icon = PhotoImage(file="RaspberryPI/images/Taskbar/Exit_Icon.png")
+    self.settings_icon = PhotoImage(file="RaspberryPI/images/Taskbar/Settings_Icon.png")
+    self.sensors_icon = PhotoImage(file="RaspberryPI/images/Taskbar/Sensors_Icon.png")
 
     
     self.tilt_value_text = StringVar()
